281655795358ac4fd332e2d530efc5138c64980a
Bertram .D Chabu <145387124+BertramChabu@users.noreply.github.com>
2025-06-24 15:46:32 +0300

    update

--- yoh/chat/admin.py
+++ yoh/chat/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import *
+from .models import Friendship, Message
 
-# Register your models here.
-admin.site.register(Friendship)
-admin.site.register(Message)
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'friend', 'created_at')
+    search_fields = ('user__username', 'friend__username')
 
-
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'timestamp', 'is_read')
+    list_filter = ('is_read', 'timestamp')
